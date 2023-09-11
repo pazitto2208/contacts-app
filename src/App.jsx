3e8da0ecc4fc30f87f0ad3c9c6e8562fd33a6b1c
pazitto2208 <epazitto@gmail.com>
2023-09-11 18:57:40 +0200

    application ready to production

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ContactsTable from './components/ContactTable/ContactTable'
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+
+export default function App() {
+  return (
+    <>
+    <body>
+      <h1 className='title'>Contacts Manager</h1>
+      <ContactsTable />
+    </body>
+    </>
+  )
+}
+
---
+++ src/components/ContactTable/ContactTable.jsx
@@ -0,0 +1,96 @@
+import DataGrid, { Column, Editing, FormItem, Paging, Button } from "devextreme-react/data-grid"
+import 'devextreme-react/text-area'
+import { useState, useEffect } from "react"
+
+export default function ContactsTable () {
+    const [contacts, setContacts] = useState([])
+
+    const notesEditorOptions = { height: 100 }
+    const url = 'http://localhost:5000/contacts/'
+
+    useEffect(() => {
+        fetch( url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then((resp) => resp.json())
+        .then((contacts) => {
+            setContacts(contacts)
+        })
+        .catch((err) => console.error(err))
+    }, [])
+
+    const handleNewContact = (contactToBeAdded) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(contactToBeAdded.data),
+        })
+        .then((resp) => resp.json())
+        .catch((err) => console.error(err))
+    }
+
+    const handleRemoveContact = (contactToBeDeleted) => {
+        fetch(url + contactToBeDeleted.data.id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .catch((err) => console.error(err))
+    }
+
+    const handleUpdateContact = (contactToBeEdited) => {
+        fetch(url + contactToBeEdited.oldData.id, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(contactToBeEdited.newData),
+        })
+        .then((resp) => resp.json())
+        .catch((err) => console.error(err))
+    }
+
+    return(
+        <>
+            <DataGrid
+                dataSource={contacts}
+                keyExpr="id"
+                showBorders={true}
+                onRowInserting={(contactToBeAdded) => handleNewContact(contactToBeAdded)}
+                onRowRemoving={(contactToBeDeleted) => handleRemoveContact(contactToBeDeleted)}
+                onRowUpdating={(contactToBeEdited) => handleUpdateContact(contactToBeEdited)}
+            >
+                <Paging enabled={false} />
+                <Editing
+                    mode="form"
+                    allowAdding={true}
+                    allowUpdating={true}
+                    allowDeleting={true}
+                />
+                <Column type="buttons">
+                    <Button name="edit" icon="edit" />
+                </Column>
+                <Column type="buttons">
+                    <Button name="delete" icon="trash" />
+                </Column>
+                <Column dataField="firstname" />
+                <Column dataField="lastname" />
+                <Column dataField="email" />
+                <Column dataField="address" />
+                <Column dataField="company" />
+                <Column dataField="telnumber1" />
+                <Column dataField="telnumber2" />
+                <Column dataField="note" visible={false}>
+                    <FormItem colSpan={2} editorType="dxTextArea" editorOptions={notesEditorOptions} />
+                </Column>
+            </DataGrid>
+        </>
+    )
+}
